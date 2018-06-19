f092192b62a59009781014fa264ebc3b5dcce5c9
Phi Long Le <pl.le@thecodingmachine.com>
2018-06-19 14:26:12 +0200

    login/signup

---
+++ app/config/firebase.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDZ_qDrdC1ufhDe5Zi-owISd6QH1i0xa74",
+    authDomain: "spaceconflictchat-c951f.firebaseapp.com",
+    databaseURL: "https://spaceconflictchat-c951f.firebaseio.com",
+    storageBucket: "spaceconflictchat-c951f.appspot.com",
+    messagingSenderId: "455205108733",
+    }
