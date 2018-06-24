e9daf63bb5551b382dbf7436e3a40394cb12da87
fueledbysushis <fueledbysushis@gmail.com>
2018-06-24 20:04:42 +0200

    ok

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDZ_qDrdC1ufhDe5Zi-owISd6QH1i0xa74",
+    authDomain: "spaceconflictchat-c951f.firebaseapp.com",
+    databaseURL: "https://spaceconflictchat-c951f.firebaseio.com",
+    projectId: "spaceconflictchat-c951f",
+    storageBucket: "spaceconflictchat-c951f.appspot.com",
+    messagingSenderId: "455205108733"
+    }
